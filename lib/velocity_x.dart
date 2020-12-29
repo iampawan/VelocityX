fe4dffde196af77ae342eefb3e7a1108c20887c1
iampawan <pawan221b@gmail.com>
2020-12-29 14:16:05 +0530

    bool ext added

--- lib/velocity_x.dart
+++ lib/velocity_x.dart
@@ -3,6 +3,7 @@ library velocity_x;
 export 'package:animator/animator.dart';
 export 'src/dart/guard.dart';
 export 'src/dart/timeline.dart';
+export 'src/extensions/bool_ext.dart';
 export 'src/extensions/context_ext.dart';
 export 'src/extensions/date_time_ext.dart';
 export 'src/extensions/iterable_ext.dart';
