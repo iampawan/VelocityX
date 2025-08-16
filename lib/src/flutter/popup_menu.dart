a8416f4ef8fc21a5dbf1d553b5da71fb69f4c1af
ShreemanMassTech <shreemanarjunsahu@gmail.com>
2025-08-17 00:53:30 +0530

    🐛 fix(popup_menu): fix onPopInvokedWithResult callback
    - change arguments of onPopInvokedWithResult from (_, __) to (_, _) to avoid linting error

--- lib/src/flutter/popup_menu.dart
+++ lib/src/flutter/popup_menu.dart
@@ -200,7 +200,7 @@ class VxPopupMenuState extends State<VxPopupMenu> {
   Widget build(BuildContext context) {
     return PopScope(
       canPop: true,
-      onPopInvokedWithResult: (_, __) => _hideMenu(),
+      onPopInvokedWithResult: (_, _) => _hideMenu(),
       child: GestureDetector(
         behavior: HitTestBehavior.translucent,
         child: widget.child,
